a434dac50d74abb89bda26617653af9a0d479a2b
LaszloffiSzabolcs <laszloffiszabolcs1273@gmail.com>
2023-01-19 10:55:31 +0100

    Update login.php

--- login.php
+++ login.php
@@ -21,14 +21,14 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+		$sql = "SELECT * FROM felhasznalo WHERE nev='$uname' AND jelszo='$pass'";
 
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-            if ($row['user_name'] === $uname && $row['password'] === $pass) {
-            	$_SESSION['user_name'] = $row['user_name'];
+            if ($row['nev'] === $uname && $row['jelszo'] === $pass) {
+            	$_SESSION['nev'] = $row['nev'];
             	$_SESSION['name'] = $row['name'];
             	$_SESSION['id'] = $row['id'];
             	header("Location: home.php");
