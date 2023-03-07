adbf2a39cd5da895e6c481cc7064ab8421290d13
LaszloffiSzabolcs <laszloffiszabolcs1273@gmail.com>
2023-03-07 11:19:41 +0100

    Jó már a header

--- view/layout/header.php
+++ view/layout/header.php
@@ -1,14 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="index.php?page=home">Home</a>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
+  <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Placeholder</a>
+      </li>
+    </div>
+    <ul class="navbar-nav navbar-right">
         <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <?php echo $_SESSION['username'];?>
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a href="">Kérelmek</a>
           <a class="dropdown-item" href="index.php?page=login&action=logout">Logout</a>
       </li>
     </ul>
-    </div>
+    
 </nav>
