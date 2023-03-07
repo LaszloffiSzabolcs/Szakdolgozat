22f9803693540cd1e335ce8b40c9f96e598821c6
LaszloffiSzabolcs <laszloffiszabolcs1273@gmail.com>
2023-03-07 10:39:21 +0100

    Navbar nem jó + szétbaszta a színeket

--- view/home.php
+++ view/home.php
@@ -6,11 +6,15 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 <html>
 <head>
 	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="../style.css">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-     <h1>Hello, <?php echo $_SESSION['nev']; ?></h1>
-     <a href="index.php?page=login&action=logout">Logout</a>
+     <?php
+     require "view/layout/header.php";
+     ?>
+     <br>
+     <h1>Hello, <?php echo $_SESSION['username']; ?></h1>
      <?php
 	require "view/layout/footer.php";
 	?>
