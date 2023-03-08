5ae1f303055373172767bd7bbf155bb318201958
LaszloffiSzabolcs <laszloffiszabolcs1273@gmail.com>
2023-03-08 08:44:09 +0100

    uj css

--- view/home.php
+++ view/home.php
@@ -6,8 +6,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 <html>
 <head>
 	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="../style.css">
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <link rel="stylesheet" type="text/css" href="home.css">
 </head>
 <body>
      <?php
--- view/layout/header.php
+++ view/layout/header.php
@@ -1,20 +1,30 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-sajat sticky-top">
   <a class="navbar-brand" href="index.php?page=home">Home</a>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Placeholder</a>
+        <a class="nav-link" href="">Kérelmek</a>
       </li>
     </div>
     <ul class="navbar-nav navbar-right">
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <?php echo $_SESSION['username'];?>
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a href="">Kérelmek</a>
-          <a class="dropdown-item" href="index.php?page=login&action=logout">Logout</a>
-      </li>
-    </ul>
-    
+    <div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+  <?php echo $_SESSION['username'];?>
+  </a>
+
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><a class="dropdown-item" href="#">placeholder</a></li>
+    <li><a class="dropdown-item" href="index.php?page=login&action=logout">Logout</a></li>
+  </ul>
+</div>
 </nav>
+</body>
+</html>
+
